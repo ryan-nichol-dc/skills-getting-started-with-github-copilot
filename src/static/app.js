b3379098a478eb4f6228b41bd035e56b5b899cfb
ryan-nichol-dc <ryan.nichol@datacom.com>
2025-03-20 01:10:50 +0000

    coll stuff

--- src/static/app.js
+++ src/static/app.js
@@ -25,11 +25,10 @@ document.addEventListener("DOMContentLoaded", () => {
           <p>${details.description}</p>
           <p><strong>Schedule:</strong> ${details.schedule}</p>
           <p><strong>Availability:</strong> ${spotsLeft} spots left</p>
-          <p><strong>Participants:</strong> ${
-            details.participants.length > 0
-              ? details.participants.join(", ")
-              : "No participants yet"
-          }</p>
+          <p><strong>Participants:</strong></p>
+          <ul class="participant-list">
+            ${details.participants.map(participant => `<li>${participant}</li>`).join("")}
+          </ul>
         `;
 
         activitiesList.appendChild(activityCard);
